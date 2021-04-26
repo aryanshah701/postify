effe4cf06875f1cd9d47f03f39ab6a7d23f0f1f4
Aryan Shah <aryanshah791@gmail.com>
2021-04-26 13:20:11 -0400

    Setup apollo server and express

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,37 @@
+// MikroORM imports
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import microOrmConfig from "./mikro-orm.config";
+
+// Apollo and GraphQL imports
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+
+import express from "express";
 
 const main = async () => {
   // Init the ORM and run the migrations
   const orm = await MikroORM.init(microOrmConfig);
   await orm.getMigrator().up();
 
-  // Add some posts
-  const postOne = orm.em.create(Post, { title: "My first post" });
-  // const postTwo = orm.em.create(Post, { title: "My second post" });
-  await orm.em.persistAndFlush(postOne);
-  // await orm.em.persistAndFlush(postTwo);
+  // Conifgure Express
+  const app = express();
+
+  // Configure Apollo Server for GraphQL endpoint
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+
+  // Add GraphQL endpoint
+  apolloServer.applyMiddleware({ app });
+
+  // Spin up the express server
+  app.listen(4000, () => console.log("Server listening at PORT 4000"));
 };
 
 main().catch((err) => {
