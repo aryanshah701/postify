ba413082be0a1faf4fdf147a31002c66a392a0f1
Aryan Shah <aryanshah791@gmail.com>
2021-04-26 19:25:29 -0400

    Add error handling and login to user resolver

--- src/resolvers/users.ts
+++ src/resolvers/users.ts
@@ -1,6 +1,14 @@
 import { User } from "../entities/User";
 import { MyContext } from "src/types";
-import { Resolver, Mutation, InputType, Field, Arg, Ctx } from "type-graphql";
+import {
+  ObjectType,
+  Resolver,
+  Mutation,
+  InputType,
+  Field,
+  Arg,
+  Ctx,
+} from "type-graphql";
 import argon2 from "argon2";
 
 // Input type for creating/loging a user
@@ -12,13 +20,57 @@ class UserInput {
   password: string;
 }
 
+// An error object for field errors
+@ObjectType()
+class FieldError {
+  @Field(() => String, { nullable: true })
+  field?: string;
+
+  @Field()
+  message: string;
+}
+
+// A response obeject with any errors or the user itself
+@ObjectType()
+class UserResponse {
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[];
+
+  @Field(() => User, { nullable: true })
+  user?: User;
+}
+
 @Resolver()
 export class UserResolver {
-  @Mutation(() => User, { nullable: true })
+  // Create a User
+  @Mutation(() => UserResponse)
   async register(
     @Arg("options") options: UserInput,
     @Ctx() { em }: MyContext
-  ): Promise<User | null> {
+  ): Promise<UserResponse> {
+    // Validate username and password
+    if (options.username.length < 3) {
+      return {
+        errors: [
+          {
+            field: "username",
+            message: "Sorry, the username needs to be > 2 characters.",
+          },
+        ],
+      };
+    }
+
+    if (options.password.length < 8) {
+      return {
+        errors: [
+          {
+            field: "password",
+            message: "Sorry, the password needs to be > 7 characters.",
+          },
+        ],
+      };
+    }
+
     // Hash the password
     const hashedPassword = await argon2.hash(options.password);
 
@@ -33,9 +85,66 @@ export class UserResolver {
       await em.persistAndFlush(newUser);
     } catch (err) {
       console.error(err);
-      return null;
+      // Handle different error cases
+      if (err.code === "23505") {
+        // Duplicate usernames
+        return {
+          errors: [
+            {
+              field: "username",
+              message: "Sorry, this username already exists!",
+            },
+          ],
+        };
+      } else {
+        // Generic case
+        return {
+          errors: [{ message: err.detail }],
+        };
+      }
     }
 
-    return newUser;
+    return {
+      user: newUser,
+    };
+  }
+
+  // Logging in a user
+  @Mutation(() => UserResponse)
+  async login(
+    @Arg("options") options: UserInput,
+    @Ctx() { em }: MyContext
+  ): Promise<UserResponse> {
+    // Grab the user fro the db
+    const user = await em.findOne(User, { username: options.username });
+
+    // Ensure that the user exists in the db
+    if (!user) {
+      return {
+        errors: [
+          {
+            field: "username",
+            message: "Sorry, this username does not exists",
+          },
+        ],
+      };
+    }
+
+    // Verify the provided password
+    const verified = await argon2.verify(user.password, options.password);
+    if (!verified) {
+      return {
+        errors: [
+          {
+            field: "password",
+            message: "Sorry, the provided password is invalid",
+          },
+        ],
+      };
+    }
+
+    return {
+      user,
+    };
   }
 }
