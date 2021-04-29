98ab95f5ac640a79f25ad974686d04a28808ff71
Aryan Shah <aryanshah791@gmail.com>
2021-04-29 18:02:26 -0400

    Update urql cache after loggin in and registering

--- postify-webui/src/pages/_app.tsx
+++ postify-webui/src/pages/_app.tsx
@@ -1,14 +1,80 @@
 import { ChakraProvider, ColorModeProvider } from "@chakra-ui/react";
 import theme from "../theme";
 
-import { createClient, Provider } from "urql";
+import { createClient, dedupExchange, fetchExchange, Provider } from "urql";
+import { cacheExchange, QueryInput, Cache } from "@urql/exchange-graphcache";
 import React from "react";
+import {
+  LoginMutation,
+  MeDocument,
+  MeQuery,
+  RegisterMutation,
+} from "../generated/graphql";
+import { setNestedObjectValues } from "formik";
 
+function betterUpdateQuery<Result, Query>(
+  cache: Cache,
+  qi: QueryInput,
+  result: any,
+  fn: (r: Result, q: Query) => Query
+) {
+  return cache.updateQuery(qi, (data) => fn(result, data as any) as any);
+}
 const client = createClient({
   url: "http://localhost:4000/graphql",
   fetchOptions: {
     credentials: "include",
   },
+  exchanges: [
+    dedupExchange,
+    cacheExchange({
+      keys: { UserResponse: () => null },
+      updates: {
+        Mutation: {
+          login(_result, _args, cache, _info) {
+            betterUpdateQuery<LoginMutation, MeQuery>(
+              cache,
+              { query: MeDocument },
+              _result,
+              (result, query) => {
+                if (result.login.errors) {
+                  return query;
+                }
+
+                console.log("From cache: ", result.login.user);
+                return {
+                  me: {
+                    user: result.login.user,
+                    __typename: "UserResponse",
+                  },
+                };
+              }
+            );
+          },
+          register(_result, _args, cache, _info) {
+            betterUpdateQuery<RegisterMutation, MeQuery>(
+              cache,
+              { query: MeDocument },
+              _result,
+              (result, query) => {
+                if (result.register.errors) {
+                  return query;
+                }
+
+                return {
+                  me: {
+                    user: result.register.user,
+                    __typename: "UserResponse",
+                  },
+                };
+              }
+            );
+          },
+        },
+      },
+    }),
+    fetchExchange,
+  ],
 });
 
 function MyApp({ Component, pageProps }: any) {
