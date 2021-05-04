e26437aafc7b6afa4ca566762b5b32cd902f2f43
Aryan Shah <aryanshah791@gmail.com>
2021-05-04 16:46:51 -0400

    Add textarea option to InputField component

--- postify-webui/src/components/InputField.tsx
+++ postify-webui/src/components/InputField.tsx
@@ -3,27 +3,31 @@ import {
   FormErrorMessage,
   FormLabel,
   Input,
+  Textarea,
 } from "@chakra-ui/react";
 import { useField } from "formik";
-import React, { InputHTMLAttributes } from "react";
+import React, { ComponentType, InputHTMLAttributes } from "react";
 
 type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
   name: string;
   label: string;
+  isTextarea?: boolean;
 };
 
 // A generic input field using formik and chakra
 export const InputField: React.FC<InputFieldProps> = ({
   label,
+  isTextarea,
   size: _,
   ...props
 }) => {
   const [field, { error }] = useField(props);
-
+  console.log(isTextarea);
+  let InputComponent: any = isTextarea ? Textarea : Input;
   return (
     <FormControl isInvalid={!!error}>
       <FormLabel htmlFor={props.name}>{label}</FormLabel>
-      <Input {...field} {...props} id={field.name} />
+      <InputComponent {...field} {...props} id={field.name} />
       {error ? <FormErrorMessage>{error}</FormErrorMessage> : null}
     </FormControl>
   );
