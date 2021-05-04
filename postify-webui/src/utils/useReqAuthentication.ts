5f0b5e7689213190832714170f9d2eab9de312ab
Aryan Shah <aryanshah791@gmail.com>
2021-05-04 17:11:21 -0400

    Add a custom hook that checks for user auth

---
+++ postify-webui/src/utils/useReqAuthentication.ts
@@ -0,0 +1,14 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { useMeQuery } from "../generated/graphql";
+
+// Naviagate to the login page if the user isn't authenticated
+export const useReqAuthentication = () => {
+  const [{ data, fetching }] = useMeQuery();
+  const router = useRouter();
+  useEffect(() => {
+    if (!fetching && !data?.me.user) {
+      router.replace("/login");
+    }
+  }, [fetching, data, router]);
+};
