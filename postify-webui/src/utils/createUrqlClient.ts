33bb00a695869ee91f0eb2a06d1d5a0605003845
Aryan Shah <aryanshah791@gmail.com>
2021-05-05 17:31:24 -0400

    Add cursorPagination client posts resolver to urql client

--- postify-webui/src/utils/createUrqlClient.ts
+++ postify-webui/src/utils/createUrqlClient.ts
@@ -1,19 +1,64 @@
-import { dedupExchange, fetchExchange } from "urql";
-import { cacheExchange } from "@urql/exchange-graphcache";
+import { dedupExchange, fetchExchange, stringifyVariables } from "urql";
+import { cacheExchange, NullArray, Resolver } from "@urql/exchange-graphcache";
 import {
   LoginMutation,
   MeQuery,
   MeDocument,
   RegisterMutation,
   LogoutMutation,
-  CreatePostMutation,
-  PostsQuery,
-  PostsDocument,
 } from "../generated/graphql";
 import { betterUpdateQuery } from "./betterUpdateQuery";
 import { pipe, tap } from "wonka";
 import { Exchange } from "urql";
 import Router from "next/router";
+
+// Cursor pagination exchange
+const cursorPagination = (): Resolver => {
+  return (_parent, fieldArgs, cache, info) => {
+    const { parentKey: entityKey, fieldName } = info;
+
+    // Grab all the queries with the given fieldName
+    const allFields = cache.inspectFields(entityKey);
+    const fieldInfos = allFields.filter((info) => info.fieldName === fieldName);
+    const size = fieldInfos.length;
+    if (size === 0) {
+      return undefined;
+    }
+
+    // Query server if query doesn't already exist in cache
+    const currQueryFieldKey = `${fieldName}(${stringifyVariables(fieldArgs)})`;
+    const currQueryInCache = cache.resolve(
+      cache.resolve(entityKey, currQueryFieldKey) as string,
+      "posts"
+    ) as string[];
+    info.partial = !currQueryInCache;
+
+    // Combine all the posts from the queries
+    const posts: string[] = [];
+    let hasMore = true;
+    fieldInfos.forEach((fi) => {
+      // Grab the posts from this query
+      const currPosts = cache.resolve(
+        cache.resolve(entityKey, fi.fieldKey) as string,
+        "posts"
+      ) as string[];
+      posts.push(...currPosts);
+
+      // Grab hasMore field from this query
+      const currHasMore = cache.resolve(
+        cache.resolve(entityKey, fi.fieldKey) as string,
+        "hasMore"
+      ) as boolean;
+      hasMore = hasMore && currHasMore;
+    });
+
+    return {
+      __typename: "PostsResponse",
+      posts,
+      hasMore,
+    };
+  };
+};
 
 // Global URQL error handling exchange
 export const errorExchange: Exchange = ({ forward }) => (ops$) => {
@@ -38,27 +83,18 @@ export const createUrqlClient = (ssrExchange: any) => ({
   exchanges: [
     dedupExchange,
     cacheExchange({
-      keys: { FieldError: () => null, UserResponse: () => null },
+      keys: {
+        PostsResponse: () => null,
+        FieldError: () => null,
+        UserResponse: () => null,
+      },
+      resolvers: {
+        Query: {
+          posts: cursorPagination(),
+        },
+      },
       updates: {
         Mutation: {
-          createPost(_result, _args, cache, _info) {
-            betterUpdateQuery<CreatePostMutation, PostsQuery>(
-              cache,
-              { query: PostsDocument },
-              _result,
-              (result, query) => {
-                if (!result.createPost) {
-                  return query;
-                }
-
-                // Push the new post onto the array of posts
-                query.posts.push(result.createPost);
-                console.log("New posts: ", query);
-
-                return query;
-              }
-            );
-          },
           logout(_result, _args, cache, _info) {
             betterUpdateQuery<LogoutMutation, MeQuery>(
               cache,
