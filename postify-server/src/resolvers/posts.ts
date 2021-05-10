1197606b4e7c469e3de0457dab028e77165a012e
Aryan Shah <aryanshah791@gmail.com>
2021-05-09 20:57:02 -0400

    Fix updatePost mutation id arg type to Int

--- postify-server/src/resolvers/posts.ts
+++ postify-server/src/resolvers/posts.ts
@@ -218,7 +218,7 @@ export class PostResolver {
   @Mutation(() => Post, { nullable: true })
   @UseMiddleware(ReqAuthentication)
   async updatePost(
-    @Arg("id") id: number,
+    @Arg("id", () => Int) id: number,
     @Arg("title") title: string,
     @Arg("text") text: string,
     @Ctx() { req }: MyContext
